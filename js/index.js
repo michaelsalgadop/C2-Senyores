8b8880433d9b761ae348f6d94b6ba8a85a8e3351
Michaelsp11 <michaelsalgadop11@gmail.com>
2024-12-19 14:45:23 +0100

    Optimización de código

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-import { senyores } from "../datos/senyores.js";
+import { senyores as senyoresAPI } from "../datos/senyores.js";
+
+let senyoresTodos = [...senyoresAPI];
 
 const principal = document.querySelector(".principal");
 const numeroMarcados = document.querySelector(".marcados");
@@ -9,38 +11,61 @@ const getInicial = (nombre) => {
   const inicial = partesNombre[0].length > 2 ? 0 : 1;
   return partesNombre[inicial].charAt(0);
 };
-
-for (const { nombre, foto, profesion, estado, twitter, marcado } of senyores) {
-  const senyorElemento = document
-    .querySelector(".senyor-molde")
-    .cloneNode(true);
-  senyorElemento.classList.remove("senyor-molde");
-  const nombreElemento = senyorElemento.querySelector(".nombre-senyor");
-  nombreElemento.textContent = nombre;
-  const imagen = senyorElemento.querySelector(".avatar > img");
-  imagen.src = `img/${foto}`;
-  const profesionElemento = senyorElemento.querySelector(".datos .profesion");
-  profesionElemento.textContent = profesion;
-  const estadoElemento = senyorElemento.querySelector(".datos .estado");
-  estadoElemento.textContent = estado;
-  const twitterElemento = senyorElemento.querySelector(".datos .redSocial");
-  twitterElemento.textContent = twitter;
-  const iconoElemento = senyorElemento.querySelector(".inicial");
-  iconoElemento.textContent = getInicial(nombre);
-  if (marcado) {
-    senyorElemento.classList.add("marcado");
-    ++numeroMarcados.textContent;
+const pintarSenyores = () => {
+  for (const {
+    nombre,
+    foto,
+    profesion,
+    estado,
+    twitter,
+    marcado,
+  } of senyoresTodos) {
+    const senyorElemento = document
+      .querySelector(".senyor-molde")
+      .cloneNode(true);
+    senyorElemento.classList.remove("senyor-molde");
+    const nombreElemento = senyorElemento.querySelector(".nombre-senyor");
+    nombreElemento.textContent = nombre;
+    const imagen = senyorElemento.querySelector(".avatar > img");
+    imagen.src = `img/${foto}`;
+    const profesionElemento = senyorElemento.querySelector(".datos .profesion");
+    profesionElemento.textContent = profesion;
+    const estadoElemento = senyorElemento.querySelector(".datos .estado");
+    estadoElemento.textContent = estado;
+    const twitterElemento = senyorElemento.querySelector(".datos .redSocial");
+    twitterElemento.textContent = twitter;
+    const iconoElemento = senyorElemento.querySelector(".inicial");
+    iconoElemento.textContent = getInicial(nombre);
+    if (marcado) {
+      senyorElemento.classList.add("marcado");
+    }
+    principal.append(senyorElemento);
   }
-  principal.append(senyorElemento);
-}
+};
+
+const borrarSenyores = () => {
+  const senyores = document.querySelectorAll(".senyor:not(.senyor-molde)");
+  senyores.forEach((senyor) => {
+    senyor.remove();
+  });
+};
+
+const pintarNumeroSenyores = () => {
+  numeroMarcados.textContent = senyoresTodos.filter(
+    (senyor) => senyor.marcado
+  ).length;
+};
+
+const pintarAI = () => {
+  pintarNumeroSenyores();
+  borrarSenyores();
+  pintarSenyores();
+};
+
+pintarAI();
 
 btnMarcarTodos.addEventListener("click", (e) => {
   e.preventDefault();
-  const senyores = document.querySelectorAll(".senyor:not(.senyor-molde)");
-  senyores.forEach((senyor) => {
-    if (!senyor.classList.contains("marcado")) {
-      senyor.classList.add("marcado");
-    }
-  });
-  numeroMarcados.textContent = senyores.length;
+  senyoresTodos = senyoresTodos.map((senyor) => ({ ...senyor, marcado: true }));
+  pintarAI();
 });
